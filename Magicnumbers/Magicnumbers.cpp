a40b5899183b4149d5b3f4d8558956451455859c
T-yopo <samecob62@gmail.com>
2024-02-28 08:24:54 +0000

    Replaced goto statement with do-while loop

--- Magicnumbers/Magicnumbers.cpp
+++ Magicnumbers/Magicnumbers.cpp
@@ -10,55 +10,59 @@
 #pragma comment(lib, "standaloneExitChoice.release.lib")
 #endif
 using namespace std;
+using namespace stdln;
 
 int main() {
-	
+
 	srand(static_cast<unsigned int>(time(nullptr)));
 	int magique; // magic number
 	int guessnumber; // the person's guess
 	int lowestNumber; // Lowest possible number
 	int highestNumber; // Highest possible number
 
-	
+
 	cout << "This is a fun guessing game where you must guess the magic number!!!!" << endl << endl;
 	cout << "Please choose the range you want your numbers to be between:\n\n";
 	cout << "Enter the lowest possible number: ";
 	cin >> lowestNumber;
 	cout << "\nEnter the highest possible number: ";
 	cin >> highestNumber;
-	try { if (lowestNumber > highestNumber) throw (1); }
-	catch (int errorCode) {
-		MessageBox(NULL, L"    Exeption thrown!\n ", L"Error!", MB_OK);
-		cout << "\n\n\nPress any key to exit...";
-		while (true) if (_kbhit()) return 0;
-
+	
+	if (lowestNumber > highestNumber) { 
+		MessageBoxW(NULL, L"Exception thrown!", L"Error!", MB_OK); 
+		cout << "Press any key 2 exit";
+		for (;;) {
+			if (_kbhit()) return 0;
+		}
 	}
-generate:
-	magique = rand(); // this function gets a random number
-	if (magique > highestNumber || magique < lowestNumber) goto generate;
+	
+		
+	
+	do {
+		magique = rand(); // this function gets a random number
+	} while (magique < lowestNumber || magique > highestNumber);
+
+
+
 	cout << "\nPlease enter your first guess here: ";
-	guess:
 	cin >> guessnumber;
-
-	if (guessnumber == magique) { 
-		cout << "\nWell done superhero, you guessed right!!!\n\n";
-		MessageBox(NULL, L"Well done!", L"You won! :)", MB_OK);
-		stdln::restartKey1 = 'a';
-		stdln::restartKey2 = 'A';
-		stdln::exitKey1 = 'e';
-		stdln::exitKey2 = 'E';
-		cout << "Press E to exit the game or A to go again!";
-		stdln::exitchoice(2);
-}
-
-	else {
+	while (guessnumber != magique) {
 		cout << "\nOopsy daisy!!";
 		if (guessnumber > highestNumber || guessnumber < lowestNumber) cout << "\nYour guess is outside of the range you specified, guess again!!: ";
 		else if (guessnumber > magique) cout << "\nYour guess is too high, guess again!!: ";
 		else if (guessnumber < magique) cout << "\nYour guess is too low, guess again!!: ";
-		goto guess;
+		cin >> guessnumber;
 	}
-	
+	MessageBoxW(NULL, L"Well done superhero, you guessed right!!", L"Yay!", MB_OK);
+	cout << "\n\nWell done superhero, you guessed right!!\n\n\n";
+	restartKey1 = 'A';
+	restartKey2 = 'a';
+	exitKey1 = 'E';
+	exitKey2 = 'e';
+	cout << "Press A to go again, or E to exit: ";
+	exitchoice();
 	return 0;
-
 }
+	
+
+	
