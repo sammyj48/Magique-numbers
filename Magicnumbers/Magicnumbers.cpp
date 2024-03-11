3c14ed4d958e1457e077dd2318fc37faa708ba19
Tyopo131 <samecob62@gmail.com>
2024-03-11 10:48:40 +0000

    Added saved game mode (that took a while)

--- Magicnumbers/Magicnumbers.cpp
+++ Magicnumbers/Magicnumbers.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <ctime>
 #include <Windows.h>
-#include "StandaloneExitChoice.h"
 #include <conio.h>
-#include <string>
+#include "StandaloneExitChoice.h"
+
 #ifdef _DEBUG
 #pragma comment(lib, "standaloneExitChoice.debug.lib")
 #else
@@ -11,50 +10,33 @@
 #endif
 using namespace std;
 using namespace stdln;
-
+void sandboxMode();
+void savedGame();
 int main() {
 
-	srand(static_cast<unsigned int>(time(nullptr)));
-	int magique; // magic number
-	int guessnumber; // the person's guess
-	int lowestNumber; // Lowest possible number
-	int highestNumber; // Highest possible number
+	cout << "Press H to load saved game, where the amount of tries\nit took you to guess is stored in a file, or S for sandbox mode: ";
+	while (true) {
 
+		if (_kbhit()) {
+			char keyHit = _getch();
+			if (keyHit == 'S' || keyHit == 's') {
+				cout << keyHit << "\n\n";
+				sandboxMode();
+				break;
+			}
+			else if (keyHit == 'H' || keyHit == 'h') {
+				cout << keyHit << "\n\n";
+				savedGame();
+				break;
+			}
 
-	cout << "This is a fun guessing game where you must guess the magic number!!!!" << endl << endl;
-	cout << "Please choose the range you want your numbers to be between:\n\n";
-	cout << "Enter the lowest possible number: ";
-	cin >> lowestNumber;
-	cout << "\nEnter the highest possible number: ";
-	cin >> highestNumber;
-	
-	if (lowestNumber > highestNumber) { 
-		MessageBoxW(NULL, L"Exception thrown!", L"Error!", MB_OK); 
-		cout << "Press any key 2 exit";
-		for (;;) {
-			if (_kbhit()) return 0;
 		}
+
+
+
 	}
-	
-		
-	
-	do {
-		magique = rand(); // this function gets a random number
-	} while (magique < lowestNumber || magique > highestNumber);
 
 
-
-	cout << "\nPlease enter your first guess here: ";
-	cin >> guessnumber;
-	while (guessnumber != magique) {
-		cout << "\nOopsy daisy!!";
-		if (guessnumber > highestNumber || guessnumber < lowestNumber) cout << "\nYour guess is outside of the range you specified, guess again!!: ";
-		else if (guessnumber > magique) cout << "\nYour guess is too high, guess again!!: ";
-		else if (guessnumber < magique) cout << "\nYour guess is too low, guess again!!: ";
-		cin >> guessnumber;
-	}
-	MessageBoxW(NULL, L"Well done superhero, you guessed right!!", L"Yay!", MB_OK);
-	cout << "\n\nWell done superhero, you guessed right!!\n\n\n";
 	restartKey1 = 'A';
 	restartKey2 = 'a';
 	exitKey1 = 'E';
@@ -63,6 +45,5 @@ int main() {
 	exitchoice();
 	return 0;
 }
-	
 
-	
+
